55d8060c188a344c01c72cdda993a436a8be437a
vboxuser <ronielsonoliveira@live.com>
2026-03-23 14:25:33 -0300

    migrate

--- config/database.ts
+++ config/database.ts
@@ -1,30 +1,15 @@
 import path from "path";
 
-export default ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
-
-  const connections = {
-    postgres: {
-      connection: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "blog-strapi"),
-        user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "senha"),
-        ssl: env.bool("DATABASE_SSL", false)
-          ? { rejectUnauthorized: false }
-          : false,
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
-      pool: { min: 2, max: 10 },
-    },
-  };
-
-  return {
+export default ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      connectionString: env("DATABASE_URL"),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
-  };
-};
+    pool: { min: 2, max: 10 },
+    acquireConnectionTimeout: 60000,
+  },
+});
